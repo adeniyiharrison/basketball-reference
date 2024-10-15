df688db6875217f82b5992b9510cdaf9d02095a0
adeniyiharrison <adeniyi@shipt.com>
2024-10-14 18:51:24 -0700

    figure out pandas

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -1,6 +1,7 @@
 import sys
 import logging
 import requests
+import pandas as pd
 from http import HTTPStatus
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
@@ -34,29 +35,36 @@ class Scraper:
             logging.error(f"Failed to retrieve data for {url}: {response.status_code}")
             return None
         
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
         gameSummaries = soup.find("div", class_="game_summaries")
         gameSummariesExpanded = gameSummaries.find_all("div", class_="game_summary expanded nohover")
 
+        scoresDf = instantiateScoreDataFrame()
         for gameSummary in gameSummariesExpanded:
             scores = gameSummary.find_all("tr", class_=["winner", "loser"])
             if len(scores) != 2:
                 logging.error(f"Unexpected number of scores for game: {url}")
                 continue
 
+            scoreDf = instantiateScoreDataFrame()
             for index, score in enumerate(scores):
                 teamAndScore = score.text.strip().split("\n")
                 if len(teamAndScore) < 2:
                     logging.error(f"Invalid team and score: {teamAndScore}")
                     continue
                 if index == 0:
-                    awayTeam = teamAndScore[0]
-                    awayScore = teamAndScore[1]
+                    scoreDf["away_team"] = teamAndScore[0]
+                    scoreDf["final_score_away"] = teamAndScore[1]
+                    if score["class"][0] == "winner":
+                        scoreDf["winner"] = teamAndScore[0]
                 else:
-                    homeTeam = teamAndScore[0]
-                    homeScore = teamAndScore[1]
-                
-            quarterScores = gameSummary.find_all("td", class_="center")
+                    scoreDf["home_team"] = teamAndScore[0]
+                    scoreDf["final_score_home"] = teamAndScore[1]
+                    if score["class"][0] == "winner":
+                        scoreDf["winner"] = teamAndScore[0]
+            scoresDf = pd.concat([scoresDf, scoreDf.iloc[[0]]])
+            # quarterScores = gameSummary.find_all("td", class_="center")
+        print("done for the day")
 
 
             
@@ -69,10 +77,32 @@ class Scraper:
             self.retreiveScoreData(currentDate)
             currentDate += timedelta(days=1)
 
+def instantiateScoreDataFrame() -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            # "date": pd.Series(dtype="datetime"),
+            "home_team": pd.Series(dtype="str"),
+            "away_team": pd.Series(dtype="str"),
+            "winner": pd.Series(dtype="str"),
+            "final_score_home": pd.Series(dtype="int"),
+            "final_score_away": pd.Series(dtype="int"),
+            "h1": pd.Series(dtype="int"),
+            "a1": pd.Series(dtype="int"),
+            "h2": pd.Series(dtype="int"),
+            "a2": pd.Series(dtype="int"),
+            "h3": pd.Series(dtype="int"),
+            "a3": pd.Series(dtype="int"),
+            "h4": pd.Series(dtype="int"),
+            "a4": pd.Series(dtype="int"),
+            "hot": pd.Series(dtype="int"),
+            "aot": pd.Series(dtype="int"),
+        }
+    )
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.DEBUG,
-        format='%(asctime)s %(message)s'
+        format="%(asctime)s %(message)s"
     )
     kwargs = dict(arg.split("=") for arg in sys.argv[1:])
     startDate = kwargs["start_date"]
